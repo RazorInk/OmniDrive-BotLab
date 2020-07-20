e942d4980ac5c33d45af13194d41d4442cebcf24
Chin-Wei Lin <pikelcw@umich.edu>
2020-07-20 17:31:43 -0500

    compile.sh

--- serialtesting/LCM_serial/compile_testUART.sh
+++ serialtesting/LCM_serial/compile_testUART.sh
@@ -1,3 +1,6 @@
+cd lcmtypes
+make
+cd ..
 g++ `pkg-config --libs lcm` -o  lcmListener customSerial.c lcmtypes/omnibot_speed_command_t.c messaging.cpp lcmListener.cpp
 g++ `pkg-config --libs lcm` -o  lcmPublisher lcmtypes/omnibot_speed_command_t.c lcmPublisher.cpp
 
