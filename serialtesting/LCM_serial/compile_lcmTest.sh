31acb77d35e11eb457d7f836d4e4fb0818f09493
Chin-Wei Lin <pikelcw@umich.edu>
2020-07-22 14:38:52 -0500

    Update compile.sh

--- serialtesting/LCM_serial/compile_lcmTest.sh
+++ serialtesting/LCM_serial/compile_lcmTest.sh
@@ -5,15 +5,18 @@ cd ..
 # g++ -c messaging.cpp
 # cd ../../serialtesting/LCM_serial
 # g++ `pkg-config --libs lcm` ../../messaging/messaging.o -o  lcmListener customSerial.c lcmListener.cpp
-g++ `pkg-config --libs lcm` -o  serialOUT -I../../lib/messaging/ \
+g++ `pkg-config --libs lcm` -o  bin/serialOUT -I../../lib/messaging/ \
 	../../lib/messaging/messaging.cpp \
 	../../lib/messaging/nu2pi.h \
 	../../lib/messaging/pi2nu.h \
 	customSerial.c serialOUT.cpp
-g++ `pkg-config --libs lcm` -o  serialIN -I../../lib/messaging/ \
+g++ `pkg-config --libs lcm` -o  bin/serialIN -I../../lib/messaging/ \
 	../../lib/messaging/messaging.cpp \
 	../../lib/messaging/nu2pi.h \
 	../../lib/messaging/pi2nu.h \
 	customSerial.c serialIN.cpp
 
+g++ `pkg-config --libs lcm` -o  bin/odometry odometry.cpp
+g++ `pkg-config --libs lcm` -o  bin/motion_controller motion_controller.cpp
+g++ `pkg-config --libs lcm` -o  bin/drive_square drive_square.cpp
 
