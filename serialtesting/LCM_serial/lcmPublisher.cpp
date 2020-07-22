6f65640061237b3059eef56980e308250e25be18
Karthik urs <ursk@umich.edu>
2020-07-22 09:20:49 -0400

    had a weird bug where it was only doing 3 sides of the square but fixed that!

--- serialtesting/LCM_serial/lcmPublisher.cpp
+++ serialtesting/LCM_serial/lcmPublisher.cpp
@@ -6,44 +6,67 @@
 
 int main(int argc, char** argv)
 {
-    lcm::LCM lcmInstance;
-    int time = 0;
-    while(true)
-    {
-        lcmInstance.handleTimeout(5000);  // higher larger rec
+	lcm::LCM lcmInstance;
+	int time = 0;
+	while(true)
+	{
+		lcmInstance.handleTimeout(500);  // higher larger rec
 
-            omnibot_speed_command_t cmd;
-	    switch(time){
-		    case 0:
-		    	cmd.v_x = 1;
-	    		cmd.v_y = 0;
-	    		cmd.w_z = 0;
+		omnibot_speed_command_t cmd;
+		switch(time){
+		case 0:
+			cmd.v_x = 1;
+			cmd.v_y = 0;
+			cmd.w_z = 0;
 			time ++;
 			break;
-		    case 1:
+		case 1:
 			cmd.v_x = 0;
-	    		cmd.v_y = 1;
-	    		cmd.w_z = 0;
+			cmd.v_y = 0;
+			cmd.w_z = 0;
 			time ++;
 			break;
-		    case 2:
+		case 2:
+			cmd.v_x = 0;
+			cmd.v_y = 1;
+			cmd.w_z = 0;
+			time ++;
+			break;
+		case 3:
+			cmd.v_x = 0;
+			cmd.v_y = 0;
+			cmd.w_z = 0;
+			time ++;
+			break;
+		case 4:
 			cmd.v_x = -1;
-	    		cmd.v_y = 0;
-	    		cmd.w_z = 0;
+			cmd.v_y = 0;
+			cmd.w_z = 0;
 			time ++;
 			break;
-		    case 3:
+		case 5:
 			cmd.v_x = 0;
-	    		cmd.v_y = -1;
-	    		cmd.w_z = 0;
+			cmd.v_y = 0;
+			cmd.w_z = 0;
+			time ++;
+			break;
+		case 6:
+			cmd.v_x = 0;
+			cmd.v_y = -1;
+			cmd.w_z = 0;
+			time++;
+			break;
+		case 7:
+			cmd.v_x = 0;
+			cmd.v_y = 0;
+			cmd.w_z = 0;
 			time = 0;
 			break;
+		}
 
-	    }
+					lcmInstance.publish("OMNIBOT_SPEED_COMMAND", &cmd);
+		std::cout<< "Published!\n";
+	}
 
-            lcmInstance.publish("OMNIBOT_SPEED_COMMAND", &cmd);
-	    std::cout<< "Published!\n";
-    }
-
-    return 0;
+	return 0;
 }
